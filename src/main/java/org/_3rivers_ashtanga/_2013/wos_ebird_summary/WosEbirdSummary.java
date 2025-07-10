05eefefc58c0a5ddeaf1f19a89dba57a8c2ebdfd
Patrick Paulson <87431250+patrick3rivers@users.noreply.github.com>
2025-07-10 09:21:05 -0700

    sketched out

--- src/main/java/org/_3rivers_ashtanga/_2013/wos_ebird_summary/WosEbirdSummary.java
+++ src/main/java/org/_3rivers_ashtanga/_2013/wos_ebird_summary/WosEbirdSummary.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
@@ -14,15 +16,12 @@ import javax.xml.parsers.ParserConfigurationException;
 import org._3rivers_ashtanga._2013.jaxb.jaxb_app.JAXBAppClass;
 import org._3rivers_ashtanga._2013.jaxb.jaxb_app.JAXBAppFactoryClass;
 import org._3rivers_ashtanga._2013.wos_ebird_summary.input_parameters.ParametersType;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbookFactory;
-import org.apache.poi.hssf.usermodel.HSSFWorkbookFactory;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
-import org.apache.poi.ss.usermodel.Cell;
 import org.w3c.dom.NamedNodeMap;
 import org.xml.sax.SAXException;
 
@@ -69,6 +68,26 @@ public class WosEbirdSummary
 	}
     }
 
+    private final XSSFWorkbook ebirdDataWorkbook;
+    private final XSSFWorkbook summaryResultWorkbook;
+    private final Map<Pair<String, String>,
+	    File> tripDataRowMap = new HashMap<>();
+    private final Map<String,
+	    Integer> speciesRowMap = new HashMap<>();
+
+    public Map<Pair<String, String>,
+	    File> getTripDataRowMap() {
+	return tripDataRowMap;
+    }
+
+    public XSSFWorkbook getEbirdDataWorkbook() {
+	return ebirdDataWorkbook;
+    }
+
+    public XSSFWorkbook getSummaryResultWorkbook() {
+	return summaryResultWorkbook;
+    }
+
     private WosEbirdSummary(
 	URI baseUri,
 	ParametersType parameters,
@@ -76,6 +95,109 @@ public class WosEbirdSummary
 	NamedNodeMap attMap)
 	throws IOException {
 	super(baseUri, parameters, marshaller, attMap);
+	File resultWorkbookFile = new File(
+		this.getParametersURI()
+			.resolve(this.getParameters()
+				.getSummaryOutput()));
+	File sourceWorkbookFile = new File(
+		this.getParametersURI()
+			.resolve(this.getParameters()
+				.getWorkbookSource()));
+	File ebirdDataWorkbookFile = new File(
+		this.getParametersURI()
+			.resolve(this.getParameters()
+				.getEbirdDataWorkbook()));
+	resultWorkbookFile.getParentFile().mkdirs();
+	try {
+	    Files.copy(sourceWorkbookFile.toPath(),
+		    resultWorkbookFile.toPath(),
+		    StandardCopyOption.REPLACE_EXISTING);
+	    // We're going to copy source workbook to result to start
+	    this.summaryResultWorkbook = new XSSFWorkbook(
+		    new FileInputStream(
+			    resultWorkbookFile));
+	    this.ebirdDataWorkbook = new XSSFWorkbook(
+		    new FileInputStream(
+			    ebirdDataWorkbookFile));
+	    initializeTripDataRows();
+	    initializeSpeciesRows();
+	} catch (IOException e) {
+	    throw new RuntimeException(e);
+	}
+
+    }
+
+    private void initializeSpeciesRows() {
+	int i = 0;
+	for (Row outputRow : this.getSummaryResultWorkbook()
+		.getSheetAt(0)) {
+	    // First two rows are heading rows
+	    if (i > 1) {
+		String value = outputRow.getCell(0)
+			.getStringCellValue().trim();
+		if (!value.isBlank())
+		    this.getSpeciesRowMap()
+			    .put(outputRow.getCell(0)
+				    .getStringCellValue()
+				    .trim(), i);
+	    }
+	    ++i;
+	}
+    }
+
+    private void initializeTripDataRows() {
+	File ebirdDataRootDir = new File(getParameters()
+		.getEbirdCompilationRootDir());
+	int tripIndex = -1;
+	int dayIndex = -1;
+	int dataWbkIdx = -1;
+
+	for (Row row : getEbirdDataWorkbook()
+		.getSheetAt(0)) {
+	    if (tripIndex < 0) {
+		// This is first row
+		int col = 0;
+		for (Cell cell : row) {
+		    if ("Trip".equals(cell
+			    .getStringCellValue().trim())) {
+			tripIndex = col;
+			if (dayIndex >= 0
+				&& dataWbkIdx >= 0)
+			    break;
+		    } else if ("Day".equals(cell
+			    .getStringCellValue().trim())) {
+			dayIndex = col;
+			if (tripIndex >= 0
+				&& dataWbkIdx >= 0)
+			    break;
+		    } else if ("Trip Report Compilation Output"
+			    .equals(cell
+				    .getStringCellValue()
+				    .trim())) {
+			dataWbkIdx = col;
+			if (tripIndex >= 0 && dayIndex >= 0)
+			    break;
+		    }
+		}
+	    } else {
+		File ebirdDataWbkFile = null;
+		String dataWbkPath = row.getCell(dataWbkIdx)
+			.getStringCellValue();
+		if (!dataWbkPath.isBlank())
+		    ebirdDataWbkFile = new File(
+			    ebirdDataRootDir,
+			    dataWbkPath.trim());
+		this.getTripDataRowMap().put(Pair.of(
+			row.getCell(tripIndex)
+				.getStringCellValue()
+				.trim(),
+			row.getCell(dayIndex)
+				.getStringCellValue()
+				.trim()),
+			ebirdDataWbkFile);
+
+	    }
+	}
     }
 
     static public void main(String[] args)
@@ -90,44 +212,84 @@ public class WosEbirdSummary
     @Override
     public void run() {
 	// We're going to copy source workbook to result to start
-	File resultWorkbookFile = new File(
-		this.getParametersURI()
-			.resolve(this.getParameters()
-				.getSummaryOutput()));
-	File sourceWorkbookFile = new File(
-		this.getParametersURI()
-			.resolve(this.getParameters()
-				.getWorkbookSource()));
-	File ebirdDataWorkbookFile = new File(this.getParametersURI().resolve(
-		this.getParameters().getEbirdDataWorkbook()));
-	resultWorkbookFile.getParentFile().mkdirs();
+	XSSFSheet resultSheet = getSummaryResultWorkbook()
+		.getSheetAt(0);
+	XSSFRow tripRow = resultSheet.getRow(0);
+	XSSFRow dayRow = resultSheet.getRow(1);
+	String tripName = null;
+	String tripDay = null;
+	for (Cell tripCell : tripRow) {
+	    int columnIndex = tripCell.getColumnIndex();
+	    String tripCellValue = tripCell
+		    .getStringCellValue();
+	    if (columnIndex > 0
+		    && !tripCellValue.isBlank()) {
+		tripName = tripCellValue.trim();
+	    }
+	    if (tripName != null) {
+		tripDay = dayRow.getCell(columnIndex)
+			.getStringCellValue().trim();
+		File dataWbkFile = getTripDataRowMap()
+			.get(Pair.of(tripName, tripDay));
+		String tripDesc = tripName + "-" + tripDay;
+		System.out.println("Processing trip: "
+			+ tripDesc + " ...");
+		summarizeEbirdData(columnIndex,
+			dataWbkFile);
+	    }
+	}
+    }
+
+    private void summarizeEbirdData(
+	int tripColumnIndex,
+	File dataWbkFile) {
+	if (dataWbkFile == null) {
+	    System.err.println("No trip data!");
+	    return;
+	}
+	if (!dataWbkFile.exists()) {
+	    System.err.println("No data file: "
+		    + dataWbkFile.getAbsolutePath());
+	    return;
+	}
 	try {
-	    Files.copy(sourceWorkbookFile.toPath(),
-		    resultWorkbookFile.toPath(),StandardCopyOption.REPLACE_EXISTING);
-	    // We're going to copy source workbook to result to start
-	    XSSFWorkbook result = new XSSFWorkbook(new FileInputStream(
-			    resultWorkbookFile));
-	    XSSFWorkbook ebirdDataWorkbook
-	    = new XSSFWorkbook(new FileInputStream(ebirdDataWorkbookFile));
-	    XSSFSheet resultSheet = result.getSheetAt(0);
-	    XSSFRow tripRow = resultSheet.getRow(0);
-	    XSSFRow dayRow = resultSheet.getRow(1);
-	    String tripName = null;
-	    String tripDay = null;
-	    for (Cell tripCell : tripRow) {
-		int columnIndex = tripCell.getColumnIndex();
-		String tripCellValue = tripCell.getStringCellValue();
-		if (tripCell.getColumnIndex() > 0
-			&& !tripCellValue.isBlank()) {
-		    tripName = tripCellValue;
+	    for (TripSummaryData speciesData : new TripSummaryDataSource(
+		    dataWbkFile)) {
+		String ebirdSpecies = speciesData
+			.getSpecies();
+		Integer speciesRow = getSpeciesRowMap()
+			.get(ebirdSpecies);
+		if (speciesRow == null) {
+		    System.err.print(
+			    "No Species corresponding to Ebird value: "
+				    + ebirdSpecies);
+		    String catVal = speciesData
+			    .getCategory();
+		    if (!catVal.isBlank())
+			System.err.print(" (Category: "
+				+ catVal.trim() + ")");
+		    System.err.println();
+		    return;
 		}
-		if (tripName != null) {
-		    tripDay = dayRow.getCell(columnIndex).getStringCellValue();
-		}
+		Cell outputCell = getOutputCell(speciesRow,tripColumnIndex);
+		outputCell.setCellValue(speciesData.getCount());
 	    }
 	} catch (IOException e) {
-	    throw new RuntimeException(e);
+	    System.err
+		    .println("error processing data file : "
+			    + dataWbkFile + "\n"
+			    + e.getMessage());
 	}
+    }
 
+    private Cell getOutputCell(
+	Integer speciesRow,
+	int tripColumnIndex) {
+	return this.getSummaryResultWorkbook().getSheetAt(0)
+		.getRow(speciesRow).getCell(tripColumnIndex);
+    }
+
+    private Map<String, Integer> getSpeciesRowMap() {
+	return this.speciesRowMap;
     }
 }
